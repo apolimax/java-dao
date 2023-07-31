3ecfd9b720d7bd779461046a099e80ea819716d0
Will Apolinário <willieapolinario@gmail.com>
2023-07-31 10:03:20 -0300

    created sellerDAO implementation and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,8 +12,8 @@ public class Program {
 	public static void main(String[] args) {
 		Department dep = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0,dep);
-		System.out.println(dep);
-		System.out.println(seller);
+		
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 
 	}
 
